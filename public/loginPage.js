9a989e751910d35865993da306abbb3a0fdd391c
MadDolph <kurbatov@it-matika.pro>
2023-02-27 00:20:44 +0300

    logIN and registration

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,1 +1,12 @@
 "use strict";
+const userForm = new UserForm();
+userForm.loginFormCallback = (data) => {
+    ApiConnector.login({login: data.login, password: data.password}, response => {        
+        response.success ? window.location.reload() : userForm.setLoginErrorMessage(response.error);
+    });        
+}
+userForm.registerFormCallback = (data) => {
+    ApiConnector.register({login: data.login, password: data.password}, response => {
+        response.success ? window.location.reload() : userForm.setLoginErrorMessage(response.error);
+    });
+}
