af8e32bb573515ea51653a0ea45d3e9583b4c3b6
MadDolph <kurbatov@it-matika.pro>
2023-02-28 16:44:02 +0300

    Favorites

--- public/homePage.js
+++ public/homePage.js
@@ -1,6 +1,7 @@
 const logOut = new LogoutButton();
 const rBoard = new RatesBoard();
 const mManager = new MoneyManager();
+const cFavorites = new FavoritesWidget();
 logOut.action = () => {
     ApiConnector.logout(response => {
         response.success ? window.location.reload() : console.log("logout failed");
@@ -11,10 +12,10 @@ ApiConnector.current(response => {
 });
 function getCurrency() {
     ApiConnector.getStocks(response => {
-        if(response) {
+        if(response.success) {
             rBoard.clearTable();
             rBoard.fillTable(response.data);
-        }
+        }        
     });
 }
 getCurrency();
@@ -30,7 +31,6 @@ mManager.addMoneyCallback = (data) => {
     });
 }
 mManager.conversionMoneyCallback = (data) => {
-    console.log(data);
     ApiConnector.convertMoney({fromCurrency: data.fromCurrency, targetCurrency: data.targetCurrency, fromAmount: data.fromAmount}, response => {
         if(response.success) {
             ProfileWidget.showProfile(response.data);
@@ -40,6 +40,24 @@ mManager.conversionMoneyCallback = (data) => {
         }        
     });
 }
+mManager.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney({to: data.to, currency: data.currency, amount: data.amount}, response => {
+        if(response.success) {
+            ProfileWidget.showProfile(response.data);
+            mManager.setMessage(response.success, "Операция выполнена успешно!");    
+        } else {
+            mManager.setMessage(response.success, response.error);    
+        }        
+    });
+}
+ApiConnector.getFavorites(response => {
+    if(response.success) {
+        cFavorites.clearTable();
+        cFavorites.fillTable(response.data);
+        cFavorites.updateUsersList();
+    }
+});
+
 
 
 
